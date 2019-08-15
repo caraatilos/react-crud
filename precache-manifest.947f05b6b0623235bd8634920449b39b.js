4911a205a72987a0fc9e9110e52a8e651671354a
caraatilos <caraatilos@gmail.com>
2019-08-15 17:59:39 +1200

    Updates

---
+++ precache-manifest.947f05b6b0623235bd8634920449b39b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "528d19a5081ff95cc792c0667d8abe6e",
+    "url": "/react-crud/index.html"
+  },
+  {
+    "revision": "97d633ccfbd2258b1886",
+    "url": "/react-crud/static/css/main.bbc27697.chunk.css"
+  },
+  {
+    "revision": "d785de2f7831aa7455e9",
+    "url": "/react-crud/static/js/2.845eef6f.chunk.js"
+  },
+  {
+    "revision": "97d633ccfbd2258b1886",
+    "url": "/react-crud/static/js/main.265d9ea1.chunk.js"
+  },
+  {
+    "revision": "2f3d0c7cc7993cab9d36",
+    "url": "/react-crud/static/js/runtime~main.73d447a4.js"
+  }
+]);
